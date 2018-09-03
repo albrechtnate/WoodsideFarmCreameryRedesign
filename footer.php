d198b0591df15c7a515d11e8d8eb59fa1db565e2
Nathaniel Albrecht <nathanielalbrecht@gmail.com>
2018-09-03 16:19:46 -0400

    First pass at converting PHP files to Static HTML

--- footer.php
+++
@@ -1,7 +0,0 @@
-		<footer class="main-footer">
-            <?php dynamic_sidebar( 'footer' ); ?>
-            <p class="main-footer__copyright">Copyright 2017 Woodside Farm Creamery</p>
-		</footer>
-        <?php wp_footer(); ?>
-    </body>
-</html>
