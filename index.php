bfd273cff499bc076d0f3c473725bee4ad8af1a7
Nathaniel Albrecht <nathanielalbrecht@gmail.com>
2018-09-17 22:04:34 -0400

    Converted WP/PHP files to Static HTML (#3)
    * First pass at converting PHP files to Static HTML
    
    * Completed WP/PHP to Static HTML Coversion
    
    * Removed the Docs folder which had the WP/SQL content since it is now integrated into the HTML.
    
    * Renamed CSS File and Updated HTML Links to Match
    
    * First pass at Sass Conversion
    
    • Created styles.scss
    • Broke styles.scss up into a number of partials
    • Created variables for the colors and some breakpoints
    
    * Reorganize into src and add npm pagackage with scripts
    
    * Package Lock update as a result of running npm install
    
    * Nested selectors in all the files and added one mixin
    
    * More Vars, Px->Rem Function, Typography Mixin
    
    - Created vars so I define the path to the image folder and font folder once
    - Created a function to convert pixels to rems
    - Created a mixin that would take a pixel font size and line-height and create rem and unitless versions
    - Converted my existing rem values back into pixels and implemented the typography mixin
    
    * Converted li.separator in menus to ::after as noted in Pull Request #1 Comment
    
    Closes #1

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-		<main>
-            <h2>Look&rsquo;s Like Your Lost…</h2>
-            <p class="btn btn--primary"><a href="<?php echo get_home_url(); ?>">Return Home</a></p>
-		</main>
-
-<?php get_footer(); ?>
